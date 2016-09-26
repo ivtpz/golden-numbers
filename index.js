bba6e118f96eee2ebc1a4747a176728341aa85d5
Ivey Topaz <iveytopaz@gmail.com>
2016-09-25 23:20:18 -0400

    removing tiles working

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ var rows = _.range(0, 5);
 
 var path = [];
 
-var start = false;
+var fibs = [1, 1];
+for (var i = 0; i < 10; i++) {
+  fibs.push(fibs[i] + fibs[i + 1]);
+}
 
 var cells = [];
 for (var i = 0; i < 6; i++) {
   cells.push([]);
   for(var j = 0; j < 6; j++)
     cells[i].push({
-      value: 'one',
+      value: randomFib(),
       loc: [i, j],
+      id: i.toString() + j.toString(),
       clicked: false,
       clickable: true
     })
 }
-console.log(cells)
 
 var rowSelction = table.selectAll('tr')
   .data(rows)
@@ -45,40 +48,76 @@ var cellSelection = rowSelction.selectAll('td')
     }
   })
   .on('click', function(d) {
-    d.clicked = !d.clicked;
-    if (d.clicked) {
-      d3.select(this)
-      .style('background', 'mediumturquoise')
-      setClickOptions(d)
-    } else {
-      d3.select(this)
-      .style('background', 'darkgrey')
-      setClickOptions(d)
+    console.log(path)
+    if (d.clickable) {
+      if (!d.clicked) {
+        if(d.value === fibs[path.length]) {
+          d3.select(this)
+          .style('background', 'mediumturquoise')
+          setClickOptions(d)
+        }
+      } else {
+        d3.select(this)
+        .style('background', 'darkgrey')
+        setClickOptions(d)
+      }
     }
   })
 
 
-var cellColor = cellSelection.append('div')
-  .style('background-image', 'url(one.png)')
+var cellColor = cellSelection
+  .append('div')
+  .style('background-image', (d) => `url(${d.value}.png)`)
 
-function success () {
-  cellColor.style('background-image', 'url(oneg.png)');
+function success (cells) {
+    d3.selectAll('div')
+    .data(cells, function(d) {return d.id})
+    .style('background-image', (d) => `url(${d.value}g.png)`);
+
+}
+
+function replaceCells () {
+  //take out success cells
+  d3.selectAll('div')
+  .data(path, (d) => d.id)
+  .remove()
 }
 
 function reset () {
-  cellColor.style('background-image', 'url(one.png)');
+  replaceCells();
+  path = [];
+  cells.forEach((row) => {
+    row.forEach((cell) => {
+      cell.clickable = true;
+      cell.clicked = false;
+    })
+  })
+  cellColor.style('background-image', (d) => `url(${d.value}.png)`);
+  cellSelection.style('background', 'grey');
 }
 
 function setClickOptions (item) {
-  if (!start) {
-    start = true;
-  }
+  item.clicked = !item.clicked;
   //if item was turned on, only allow clicking in adjacent squares
   if (item.clicked) {
-    path.push(item.loc)
+    path.push(item);
+    if (path.length >= 3) {
+      success(path);
+      setTimeout(reset, 1000)
+    }
     restrictToSurrounding(item);
   } else {
-  //if item was turned off, back up in path, reset clickable
+  //go back one step in path
+    path.pop();
+    if (path.length !== 0) {
+      restrictToSurrounding(path[path.length - 1])
+    } else {
+      cells.forEach((row) => {
+        row.forEach((cell) => {
+          cell.clickable = true;
+        })
+      })
+    }
   }
 };
 
@@ -86,13 +125,27 @@ function restrictToSurrounding (item) {
   cells.forEach((row) => {
     row.forEach((cell) => {
       if (cell.loc[0] >= item.loc[0] - 1 && cell.loc[0] <= item.loc[0] + 1 && cell.loc[1] >= item.loc[1] - 1 && cell.loc[1] <= item.loc[1] + 1) {
+        cell.clickable = true;
         if (cell.loc[0] + cell.loc[1] === item.loc[0] + item.loc[1] || cell.loc[0] - cell.loc[1] === item.loc[0] - item.loc[1]) {
           cell.clickable = false;
           item.clickable = true;
+          if (path.length > 1) {
+            path[path.length - 2].clickable = false;
+          }
         }
       } else {
         cell.clickable = false;
       }
     })
   })
+}
+
+
+function randomFib () {
+  var percent = Math.ceil(Math.random() * 100);
+  if (percent < 50) return fibs[1];
+  if (percent < 70) return fibs[2];
+  if (percent < 85) return fibs[3];
+  if (percent < 95) return fibs[4];
+  return fibs[5];
 }
